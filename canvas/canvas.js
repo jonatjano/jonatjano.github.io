e9b60624d3d410bcbde3eaf5ce1527101bb04ca2
jonatjano <jonatjano@laposte.net>
2016-02-18 18:59:29 +0100

    Update canvas.js

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -191,6 +191,7 @@ imgCase[0].onload = affichageTerrainCombat();
 	function mortJoueur()
 	{
 		timeOutInterval = [];
+		monstreCombat = [];
 		alert("t'es mort, tu auras plus de chance la prochaine fois");
 		alert("tu as reussi a tuer " + monstreCombat[0][5] + " monstres")
 		for (var i = 0; i < joueur.length; i++) {
@@ -199,12 +200,6 @@ imgCase[0].onload = affichageTerrainCombat();
 		for (var i = 0; i < monstreCombat.length; i++) {
 			map[monstreCombat[i][0]][monstreCombat[i][1]][1] = Math.abs(map[monstreCombat[i][0]][monstreCombat[i][1]][1]);
 		}
-		joueur[0][0] = 8;
-		joueur[0][1] = 1;
-		joueur[0][4] += 5;
-		monstreCombat[0][0] = 15;
-		monstreCombat[0][1] = 22;
-		monstreCombat[0][2] = monstreCombat[0][3];
 		affichageTerrainCombat();
 		affichageRecapitulatif();
 		return joueur[0][2] - joueur[0][3];
@@ -212,24 +207,14 @@ imgCase[0].onload = affichageTerrainCombat();
 
 	function mortMonstre()
 	{
-		alert("Tu l'as éclaté super, maintenant plus dur");
+		timeOutInterval = [];
+		monstreCombat = [];
 		for (var i = 0; i < joueur.length; i++) {
 			map[joueur[i][0]][joueur[i][1]][1] = Math.abs(map[joueur[i][0]][joueur[i][1]][1]);
 		}
 		for (var i = 0; i < monstreCombat.length; i++) {
 			map[monstreCombat[i][0]][monstreCombat[i][1]][1] = Math.abs(map[monstreCombat[i][0]][monstreCombat[i][1]][1]);
 		}
-		joueur[0][0] = 8;
-		joueur[0][1] = 1;
-		joueur[0][2] = joueur[0][3];
-		for (var i = 0; i < monstreCombat.length; i++) {
-			monstreCombat[i][0] = 15 - i;
-			monstreCombat[i][1] = 22 - i;
-		}
-		monstreCombat[0][3] += 5;
-		monstreCombat[0][5] += 1;
-		monstreCombat[0][4] += (monstreCombat[0][5] % 2 == 0)? 1: 0
-		monstreCombat[0][6] += (monstreCombat[0][6] < 4)? 1 : 0;
 		affichageTerrainCombat();
 		affichageRecapitulatif();
 		return monstreCombat[0][2] - monstreCombat[0][3];
